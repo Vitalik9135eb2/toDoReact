41b34b95b0fc574bee6bd0a932f20fe0ea869bb3
Vitalik <05vp94@mail.ru>
2022-09-25 11:57:45 +0300

    done 100% logic

--- src/components/listTodo.jsx/ListTodo.jsx
+++ src/components/listTodo.jsx/ListTodo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import s from "./_listTodo.module.scss" 
+import { useState } from "react";
+import s from "./_listTodo.module.scss"
 
 
 const ListTodo = ({ todo, setTodo }) => {
 
+    const [edit, setEdit] = useState(null)
+    const [value, setValue] = useState("")
+
     const deleteTodo = (id) => {
-        console.log("xyi" + id)
         let newTodo = [...todo].filter(item => item.id != id)
         setTodo(newTodo)
     }
@@ -13,7 +16,7 @@ const ListTodo = ({ todo, setTodo }) => {
     const statusTodo = (id) => {
         console.log("status")
         let newTodo = [...todo].filter(item => {
-            if(item.id == id) {
+            if (item.id == id) {
                 item.status = !item.status
             }
             return item
@@ -23,22 +26,56 @@ const ListTodo = ({ todo, setTodo }) => {
         console.log(todo)
     }
 
-    const editTodo = (id) => {
-
+    const editTodo = (id, value) => {
+        setEdit(id)
+        setValue(value)
     }
+
+    const saveTodo = (id) =>{
+        
+        let newTodo = [...todo].map(item => {
+            if(item.id == id){
+                item.title = value
+            }
+
+            return item
+        })
+
+        setTodo(newTodo)
+        setEdit(null)
+    }
+
 
     return (
         <div className="">
             {
                 todo.map(item => (
-                    
-                <div key={item.id} className={s.wrap}>
-                    <div>{item.title}</div>
-                    <button onClick={() => deleteTodo(item.id)}></button>
-                    <button onClick={() => statusTodo(item.id)}></button>
-                    <button onClick={() => editTodo(item.id)}></button>
-                </div>
-                
+
+                    <div key={item.id} className={s.wrap}>
+
+                        {
+                            edit == item.id
+                                ? <div>
+                                    <input type="text" onChange={e => setValue(e.target.value)} value={value} />
+                                </div>
+                                :
+                                <div>{item.title}</div>
+                        }
+
+                        {
+                            edit == item.id
+                                ? <button onClick={() => saveTodo(item.id)}>Save</button>
+
+                                : <div>
+                                    <button onClick={() => deleteTodo(item.id)}></button>
+                                    <button onClick={() => statusTodo(item.id)}></button>
+                                    <button onClick={() => editTodo(item.id, item.title)}></button>
+                                </div>
+                        }
+
+
+                    </div>
+
                 ))
             }
         </div>
